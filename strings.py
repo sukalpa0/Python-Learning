ced25a47e7ccd857c980c6268ad85a9ff9870c5b
Sukalpa Devkota <143915953+sukalpa0@users.noreply.github.com>
2026-05-10 21:22:55 +0545

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,93 @@
+# for x in 'banana':
+#     print(x)
+
+# a = 'Hello World'
+# #to check the lenth of Hello world
+# print(len(a))
+
+# txt = "I am sukalpa from uniglobe school"
+# #checks whether there is 'school' in txt or not
+# print("school" not in txt)
+
+# a = 'I am sukalpa'
+
+# print(a[2:5])
+
+
+# name = "Sukalpa"
+# # from 2 it get started and before 6 which is 5th letter it gets stopped so the output is 'kalp'
+# print(name[2:6])
+
+# name = "Sukalpa"
+# # from 2 it get started and before 6 which is 5th letter it gets stopped so the output is 'kalp'
+# print(name[2:])
+
+# name = "Sukalpa".upper()
+
+# print(name)
+
+# name = "DukalDa"
+# name = (name.replace("D", "S"))
+# print(name)
+
+# name = "Sukalpa | Devkota"
+# name = name.split('|')
+# print(name)
+
+# price = 28
+# rate = f'the price is {price:.2f} dollars '
+# print(rate)
+
+# pi = 3.1415926535
+# #we used 4f because we wanted 4 decimal after '.'
+# print(f"The value of pi is {pi:.4f}")
+
+# x = 7
+# y = 3
+# #sum of 7 + 3
+# sum = x + y
+# print(f"The sum of {x} + {y} is {sum}")
+
+# price = 48.9
+
+# print(f"The price is {price:.2f}")
+
+# score = 95
+
+# print(f"Your score is {score}")
+
+# for i in range(1,4):
+#     square = i **2
+#     print(f"The square of {i} is {square:>5}")
+
+# product1 = "Laptop"
+# price1 = 1200
+# product2 = "Phone"
+# price2 = 800
+
+# print(f"Product: {product1:<10} | Price: {price1:.2f}")
+# print(f"Product: {product2:<10} | Price: {price2:.2f}")
+
+# word = 'Python'
+# # this symbol is to make the word left aligned and give space backward
+# print(f"{word:<12}")
+
+# score = 87
+# # this is to make the text right aligned
+# print(f"{score:>6}")
+
+# name = "Sukalpa"
+# This is to make the text Center aligned
+# print(f"{name:^15}")
+
+# pi = 3.1415926535
+# #.2f helps to print the 2 decimal after the dot
+# print(f"{pi:.2f}")
+
+# Price = 49.9
+
+# print(f"${Price:.2f}")
+
+# for i in range(1,6): # this is a loop where i starts from 1 and ends in 5
+#     square = i **2 # this calculates the square of i 
+#     print(f"{i} ->{square:>4}") #prints the square with right-alligned
